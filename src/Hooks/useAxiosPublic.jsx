b69326f283d29c50a34811f58c3465538a899001
mushayed125 <kmr18adi@gmail.com>
2025-01-30 22:01:04 +0600

    baseURL has been changed in the axiosPublic hook

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://blood-link-server-five.vercel.app'
 })
 
 const useAxiosPublic = () => {
